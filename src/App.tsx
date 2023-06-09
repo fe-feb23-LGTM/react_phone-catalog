8ec962db39d2556ed4bbe3692e0b5f264a4cd787
Oleksandr Bavdyk <sbavdik@gmail.com>
2023-06-09 17:38:13 +0300

    updated version

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import './App.scss';
 import { Outlet, NavLink } from 'react-router-dom';
 
-import { Main } from './components/main';
 import { Header } from './components/Header';
 import { Footer } from './components/Footer';
 
