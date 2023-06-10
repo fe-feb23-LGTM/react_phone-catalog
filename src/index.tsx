de524b83c55a1847245d3c12e61426ebc0afdd80
kukla1989 <kukla.1989@ukr.net>
2023-06-10 09:47:18 +0300

    change header links accordig to react routers and some little improvements

--- src/index.tsx
+++ src/index.tsx
@@ -9,9 +9,13 @@ ReactDOM.render(
   <HashRouter>
     <Routes>
       <Route path="/" element={<App />}>
-        <Route path="/home" element={<Navigate to="/" />} />
         <Route index element={<h1 className="title">home page</h1>} />
+        <Route path="/home" element={<Navigate to="/" />} />
         <Route path="/phones" element={<Main />} />
+        <Route path="/tablets" element={<h1>tablets</h1>} />
+        <Route path="/accessories" element={<h1>accessories</h1>} />
+        <Route path="/favourites" element={<h1>favourites</h1>} />
+        <Route path="/cart" element={<h1 className="title">cart</h1>} />
         <Route path="*" element={<h1 className="title">Page not found</h1>} />
       </Route>
     </Routes>
