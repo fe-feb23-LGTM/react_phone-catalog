54c8c5e69b908bc55b600082fbdbdf4cdc3dc4da
Anastasiia Yuzyfyshyn <nastyajuzufichn@gmail.com>
2023-06-11 16:40:35 +0300

    finished burger menu

--- src/components/BurgerMenu/burgerMenu.tsx
+++ src/components/BurgerMenu/burgerMenu.tsx
@@ -1,107 +1,79 @@
 import { NavLink } from 'react-router-dom';
 import classNames from 'classnames';
-import { useState } from 'react';
 
 /* eslint-disable jsx-a11y/anchor-is-valid */
 export const BurgerMenu = () => {
-  const [isMenuOpen, setIsMenuOpen] = useState(true);
-
-  const toggleMenu = () => {
-    setIsMenuOpen(!isMenuOpen);
-  };
-
-  const isActive = (path: string) => {
-    return window.location.pathname === path;
-  };
-
   return (
-    <>
-      {isMenuOpen && (
-        <div className="menu">
-          <div className="menu__top">
-            <div className="menu__top__logo">
-              <NavLink to="/" className="menu__top__logo__icon">
-                Nice gadjet
-              </NavLink>
-            </div>
-            <div
-              className="menu__top__close"
-              onClick={toggleMenu}
-              role="button"
-              tabIndex={0}
-              onKeyDown={toggleMenu}
-            >
-              <span className="menu__top__close__icon">
-                X
-              </span>
-            </div>
-
-          </div>
-          <div className="menu__content">
-            <ul className="menu__content__list">
-              <li className="menu__content__item">
-                <NavLink
-                  to="/"
-                  className={classNames('menu__content__item__link', {
-                    content__active: isActive('/'),
-                  })}
-                >
-                  Home
-                </NavLink>
-              </li>
-              <li className="menu__content__item">
-                <NavLink
-                  to="/phones"
-                  className={classNames('menu__content__item__link', {
-                    active: isActive('/phones'),
-                  })}
-                >
-                  Phones
-                </NavLink>
-              </li>
-              <li className="menu__content__item">
-                <NavLink
-                  to="/tablets"
-                  className={classNames('menu__content__item__link', {
-                    active: isActive('/tablets'),
-                  })}
-                >
-                  Tablets
-                </NavLink>
-              </li>
-              <li className="menu__content__item">
-                <NavLink
-                  to="/accessories"
-                  className={classNames('menu__content__item__link', {
-                    active: isActive('/accessories'),
-                  })}
-                >
-                  Accessories
-                </NavLink>
-              </li>
-            </ul>
-          </div>
-          <div className="menu__bottom">
+    <div className="menu">
+      <div className="menu__content">
+        <ul className="menu__content__list">
+          <li className="menu__content__item">
             <NavLink
-              to="/favourites"
-              className={classNames('menu__bottom__item', {
-                bottom__active: isActive('/favourites'),
-              })}
+              to="/"
+              className={({ isActive }) => classNames(
+                'menu__content__item__link',
+                { 'is-active': isActive },
+              )}
             >
-              Favourites
+              Home
             </NavLink>
-
+          </li>
+          <li className="menu__content__item">
             <NavLink
-              to="/cart"
-              className={classNames('menu__bottom__item', {
-                bottom__active: isActive('/cart'),
-              })}
+              to="/phones"
+              className={({ isActive }) => classNames(
+                'menu__content__item__link',
+                { 'is-active': isActive },
+              )}
             >
-              Cart
+              Phones
             </NavLink>
-          </div>
-        </div>
-      )}
-    </>
+          </li>
+          <li className="menu__content__item">
+            <NavLink
+              to="/tablets"
+              className={({ isActive }) => classNames(
+                'menu__content__item__link',
+                { 'is-active': isActive },
+              )}
+            >
+              Tablets
+            </NavLink>
+          </li>
+          <li className="menu__content__item">
+            <NavLink
+              to="/accessories"
+              className={({ isActive }) => classNames(
+                'menu__content__item__link',
+                { 'is-active': isActive },
+              )}
+            >
+              Accessories
+            </NavLink>
+          </li>
+        </ul>
+      </div>
+      <div className="menu__bottom">
+        <NavLink
+          to="/favourites"
+          className={({ isActive }) => classNames(
+            'menu__bottom__item',
+            { 'is-active': isActive },
+          )}
+        >
+          Favourites
+        </NavLink>
+
+        <NavLink
+          to="/cart"
+          className={({ isActive }) => classNames(
+            'menu__bottom__item',
+            { 'is-active': isActive },
+          )}
+        >
+          Cart
+        </NavLink>
+      </div>
+    </div>
   );
 };
