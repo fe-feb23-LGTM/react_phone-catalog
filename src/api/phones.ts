88a55bd988d242ff527515c7e2bd8a354bc02547
Andrii <remote.makandriival@gmail.com>
2023-06-13 17:46:52 +0300

    funcsDone

--- src/api/phones.ts
+++ src/api/phones.ts
@@ -1,11 +1,45 @@
-/* eslint-disable no-console */
 import { Phone } from '../types/Phone';
+
+// https://backend-phone-catalog.onrender.com/phones?sortBy=year&sortOrder=DESC&from=13&to=16
 
 const phonesUrl = 'https://backend-phone-catalog.onrender.com/phones';
 
-export const getPhones = async (): Promise<Phone[]> => {
-  // const URL = `${phonesUrl}`;
+const sortBySwitch = (sortby: string) => {
+  switch (sortby) {
+    case 'newest':
+      return '?sortBy=year&sortOrder=DESC';
+    case 'oldest':
+      return '?sortBy=year&sortOrder=ASC';
+    case 'low':
+      return '?sortBy=price&sortOrder=ASC';
+    case 'high':
+      return '?sortBy=price&sortOrder=DESC';
+    default:
+      return '?sortBy=price&sortOrder=DESC';
+  }
+};
 
+export const getPhones = async (
+  sortby: string,
+  itemsOnPage: string,
+  page: string,
+): Promise<Phone[]> => {
+  const startItem = (Number(itemsOnPage) * Number(page)).toString();
+  const sortByCase = sortBySwitch(sortby);
+  const startAndQuantity = `&from=${startItem}&to=${itemsOnPage}`;
+  const URL = phonesUrl + sortByCase + startAndQuantity;
+
+  return fetch(URL)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error();
+      }
+
+      return response.json();
+    });
+};
+
+export const getAllPhones = async (): Promise<Phone[]> => {
   return fetch(phonesUrl)
     .then(response => {
       if (!response.ok) {
--- src/components/productTable/ProductTable.tsx
+++ src/components/productTable/ProductTable.tsx
@@ -1,25 +1,60 @@
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
 import { useEffect, useState } from 'react';
 import { NavLink } from 'react-router-dom';
-import { getPhones } from '../../api/phones';
+import cn from 'classnames';
+import {
+  getPhones,
+  getAllPhones,
+} from '../../api/phones';
 import { Phone } from '../../types/Phone';
 
 export const ProductTable = () => {
   const [phones, setPhones] = useState<Phone[]>([]);
   const [error, setError] = useState('');
+  const [sortBy, setSortBy] = useState('newest');
+  const [itemsOnPage, setItemsOnPage] = useState('8');
+  const [currentPage, setCurrentPage] = useState('1');
+  const [allPageCount, setAllPageCount] = useState<string[]>([]);
 
   const getPhonesFromServer = async () => {
     try {
-      const phonesFromServer: Phone[] = await getPhones();
+      const phonesFromServer: Phone[] = await getPhones(
+        sortBy,
+        itemsOnPage,
+        currentPage,
+      );
 
       setPhones(phonesFromServer);
     } catch {
-      setError('Get phones error!');
+      setError('can not phones error!');
     }
   };
 
+  const setPagination = async () => {
+    const allPhones = await getAllPhones();
+    const pageCount = allPhones.length / Number(itemsOnPage);
+    const pageCountArr = [];
+
+    // eslint-disable-next-line no-plusplus
+    for (let i = 1; i <= pageCount; i++) {
+      pageCountArr.push(i.toString());
+    }
+
+    setAllPageCount(pageCountArr);
+  };
+
+  // eslint-disable-next-line no-console
+  console.log(currentPage);
+
   useEffect(() => {
     getPhonesFromServer();
+    setPagination();
   }, []);
+
+  useEffect(() => {
+    getPhonesFromServer();
+  }, [sortBy, itemsOnPage, currentPage]);
 
   return (
     <div className="productTable__wraper">
@@ -55,9 +90,14 @@ export const ProductTable = () => {
                 name="sortBy"
                 id="sortBy"
                 className="select productTable__selects__sortby"
+                onChange={(event) => {
+                  setSortBy(event.target.value);
+                }}
               >
                 <option value="newest">Newest</option>
                 <option value="oldest">Oldest</option>
+                <option value="low">Low Price</option>
+                <option value="high">High Price</option>
               </select>
             </div>
           </label>
@@ -70,10 +110,14 @@ export const ProductTable = () => {
                 name="itemsOnPage"
                 id="itemsOnPage"
                 className="select productTable__selects__itemsOnPage"
+                onChange={(event) => {
+                  setItemsOnPage(event.target.value);
+                }}
               >
-                <option value="5">5</option>
-                <option value="10">10</option>
+                <option value="8">8</option>
                 <option value="16">16</option>
+                <option value="32">32</option>
+                <option value="64">64</option>
               </select>
             </div>
           </label>
@@ -89,72 +133,56 @@ export const ProductTable = () => {
 
         <div className="pagination_container">
           <nav
-            className="pagination is-rounded is-centered is-small"
+            className="pagination is-rounded is-small"
             role="navigation"
             aria-label="pagination"
           >
-            <a className="pagination-previous" href="/">
-              <img
-                alt="Vector(Stroke)"
-                src="icons/Vector(Stroke).svg"
-                className="pagination-prev-icon"
-              />
-            </a>
             <ul className="pagination-list">
-              <li>
-                <a
-                  className="pagination-link"
+              <li className="pagination-previous">
+                <img
+                  alt="Vector(Stroke)"
+                  src="icons/Vector(Stroke).svg"
+                  className="pagination-prev-icon"
+                  onClick={() => {
+                    if (currentPage === '1') {
+                      return;
+                    }
+
+                    setCurrentPage((prev) => (Number(prev) - 1).toString());
+                  }}
+                />
+              </li>
+
+              {allPageCount.map(page => (
+                <li
+                  key={page}
+                  className={cn('pagination-link', {
+                    'is-current': currentPage === page,
+                  })}
                   aria-label="Goto page 1"
-                  href="/"
+                  onClick={() => {
+                    setCurrentPage(page);
+                  }}
                 >
-                  1
-                </a>
-              </li>
-              <li>
-                <a
-                  className="pagination-link"
-                  aria-label="Goto page 45"
-                  href="/"
-                >
-                  2
-                </a>
-              </li>
-              <li>
-                <a
-                  className="pagination-link is-current"
-                  aria-label="Page 46"
-                  aria-current="page"
-                  href="/"
-                >
-                  3
-                </a>
-              </li>
-              <li>
-                <a
-                  className="pagination-link"
-                  aria-label="Goto page 47"
-                  href="/"
-                >
-                  4
-                </a>
-              </li>
-              <li>
-                <a
-                  className="pagination-link"
-                  aria-label="Goto page 86"
-                  href="/"
-                >
-                  5
-                </a>
+                  {page}
+                </li>
+              ))}
+
+              <li className="pagination-next">
+                <img
+                  alt="Vector(Stroke)"
+                  src="icons/Vector(Stroke).svg"
+                  className="pagination-next-icon"
+                  onClick={() => {
+                    if (currentPage === '8') {
+                      return;
+                    }
+
+                    setCurrentPage((prev) => (Number(prev) + 1).toString());
+                  }}
+                />
               </li>
             </ul>
-            <a className="pagination-next" href="/">
-              <img
-                alt="Vector(Stroke)"
-                src="icons/Vector(Stroke).svg"
-                className="pagination-next-icon"
-              />
-            </a>
           </nav>
         </div>
 
