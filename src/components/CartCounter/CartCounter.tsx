ab2e02cb787f67ee7497766adb0264a46e5f2e95
Anastasiia Yuzyfyshyn <nastyajuzufichn@gmail.com>
2023-06-14 10:32:28 +0300

    created separate components for counter

---
+++ src/components/CartCounter/CartCounter.tsx
@@ -0,0 +1,14 @@
+export const CartCounter: React.FC = () => {
+  return (
+    <div className="counter">
+      <img
+        src="icons/Shopping_bag(Cart).svg"
+        alt="cart"
+        className="counter__img"
+      />
+      <div className="counter__number">
+        1
+      </div>
+    </div>
+  );
+};
