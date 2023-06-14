e4ef0ad9a0f996463d8e45b713cecae2a707ee1d
kukla1989 <kukla.1989@ukr.net>
2023-06-14 12:30:46 +0300

    move add to cart and favourite buttons to component AddToCartFav

---
+++ src/components/AddToCartFav/AddToCartFav.tsx
@@ -0,0 +1,112 @@
+import classNames from 'classnames';
+import { useState } from 'react';
+import { Phone } from '../../types/Phone';
+
+function isPhoneAdded(id: number, to: string) {
+  if (!localStorage.getItem(to)) {
+    return false;
+  }
+
+  const phones = localStorage.getItem(to)
+    ? JSON.parse(localStorage.getItem(to) || '')
+    : [];
+
+  return phones.some((phone: Phone) => phone.id === id);
+}
+
+// isAdd if equal to true then phone will be add to 'to'
+//  togglePhone('cart', true, phone) - will add phone to localeStorage with key 'cart'
+export function togglePhone(to: string, isAdd: boolean, phone: Phone) {
+  const phones: Phone[] = localStorage.getItem(to)
+    ? JSON.parse(localStorage.getItem(to) || '')
+    : [];
+
+  if (isAdd) {
+    phones.push(phone);
+    localStorage.setItem(to, JSON.stringify(phones));
+
+    return;
+  }
+
+  const filteredPhones = phones.filter(
+    phoneFilter => phoneFilter.id !== phone.id,
+  );
+
+  localStorage.setItem(to, JSON.stringify(filteredPhones));
+}
+
+interface Props {
+  phone: Phone;
+}
+
+export const AddToCartFav: React.FC<Props> = ({ phone }) => {
+  const [isAddedToFav, setIsAddedToFav] = useState(
+    isPhoneAdded(phone.id, 'fav'),
+  );
+  const [isAddedToCart, setIsAddedToCart] = useState(
+    isPhoneAdded(phone.id, 'cart'),
+  );
+
+  const handleCardButton = () => {
+    if (isAddedToCart) {
+      setIsAddedToCart(false);
+      togglePhone('cart', false, phone);
+
+      return;
+    }
+
+    setIsAddedToCart(true);
+    togglePhone('cart', true, phone);
+  };
+
+  const handleFavButton = () => {
+    if (isAddedToFav) {
+      setIsAddedToFav(false);
+      togglePhone('fav', false, phone);
+
+      return;
+    }
+
+    setIsAddedToFav(true);
+    togglePhone('fav', true, phone);
+  };
+
+  return (
+
+    <div className="addToCartFav__buttons">
+      <button
+        className={classNames('addToCartFav__add', {
+          'addToCartFav__add--added': isAddedToCart,
+        })}
+        type="button"
+        onClick={handleCardButton}
+      >
+        {isAddedToCart ? 'Added' : 'Add to cart'}
+        {' '}
+      </button>
+
+      <button
+        onClick={handleFavButton}
+        type="button"
+        className="addToCartFav__like-button"
+      >
+        {isAddedToFav
+          ? (
+            <img
+              src="icons/Buttons/likeYellow.svg"
+              alt="like"
+              className="addToCartFav__like"
+            />
+          )
+          : (
+            <img
+              src="icons/Buttons/like.svg"
+              alt="like"
+              className="addToCartFav__like"
+            />
+          )}
+
+      </button>
+    </div>
+  );
+};
