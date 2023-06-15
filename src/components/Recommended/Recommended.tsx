922c3f3657eecdd7c8c4302b477cc9e521af57a3
Anastasiia Yuzyfyshyn <nastyajuzufichn@gmail.com>
2023-06-15 19:14:20 +0300

    fixed some problems

--- src/components/Recommended/Recommended.tsx
+++ src/components/Recommended/Recommended.tsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
+import SwiperCore, { Navigation } from 'swiper';
 import { getPhones } from '../../api/phones';
 import { Phone } from '../../types/Phone';
 import { Card } from '../Card/Card';
@@ -12,31 +13,40 @@ export const Recommended = () => {
     getPhones('price', '8', '1').then((data) => setPhones(data));
   }, []);
 
+  SwiperCore.use([Navigation]);
+
   return (
-    <Swiper
-      slidesPerView={2}
-      spaceBetween={16}
-      observer
-      observeParents
-      breakpoints={{
-        640: {
-          slidesPerView: 2,
-        },
-        768: {
-          slidesPerView: 3,
-        },
-        1024: {
-          slidesPerView: 4,
-        },
-      }}
-    >
-      {phones.map((phone) => (
-        <SwiperSlide key={phone.id}>
-          <div className="card__container">
+    <div className="swiper-container">
+      <div className="swiper-button-prev" />
+      <div className="swiper-button-next" />
+      <Swiper
+        slidesPerView={2}
+        spaceBetween={16}
+        observer
+        observeParents
+        loop
+        navigation={{
+          prevEl: '.swiper-button-prev',
+          nextEl: '.swiper-button-next',
+        }}
+        breakpoints={{
+          640: {
+            slidesPerView: 2,
+          },
+          768: {
+            slidesPerView: 3,
+          },
+          1024: {
+            slidesPerView: 4,
+          },
+        }}
+      >
+        {phones.map((phone) => (
+          <SwiperSlide key={phone.id}>
             <Card phone={phone} />
-          </div>
-        </SwiperSlide>
-      ))}
-    </Swiper>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
   );
 };
