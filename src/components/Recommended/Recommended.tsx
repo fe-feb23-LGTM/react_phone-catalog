236207697033eaffed7a85fe281d12f103a7b525
Anastasiia Yuzyfyshyn <nastyajuzufichn@gmail.com>
2023-06-16 17:36:58 +0300

    added func for recomended

--- src/components/Recommended/Recommended.tsx
+++ src/components/Recommended/Recommended.tsx
@@ -1,17 +1,56 @@
 import { useState, useEffect } from 'react';
-import { getPhones } from '../../api/phones';
-import { Phone } from '../../types/Phone';
+import { getAllPhones, getPhoneById } from '../../api/phones';
 import { Slider } from '../Slider';
+import { Phone } from '../../types/Phone';
 
 export const Recommended = () => {
-  const [phones, setPhones] = useState<Phone[]>([]);
-  const title = 'You may also like';
+  const [allPhones, setAllPhones] = useState<Phone[]>([]);
+  const [selectedPhone, setSelectedPhone] = useState<Phone>({} as Phone);
+  const [phonesPrevYear, setPhonesPrevYear] = useState<Phone[]>([]);
+  const [phonesNextYear, setPhonesNextYear] = useState<Phone[]>([]);
+  const [phonesCurrentYear, setPhonesCurrentYear] = useState<Phone[]>([]);
 
   useEffect(() => {
-    getPhones('price', '16', '1').then((data) => setPhones(data));
+    const fetchPhones = async () => {
+      const phones = await getAllPhones();
+
+      setAllPhones(phones);
+    };
+
+    const fetchPhone = async () => {
+      const phone = await getPhoneById('32');
+
+      setSelectedPhone(phone);
+    };
+
+    fetchPhones();
+    fetchPhone();
   }, []);
 
-  return (
-    <Slider selectedPhones={phones} title={title} />
-  );
+  useEffect(() => {
+    if (selectedPhone.year !== undefined) {
+      const currentYear = allPhones.filter(
+        (phone) => phone.year === selectedPhone.year,
+      );
+      const prevYear = allPhones.filter(
+        (phone) => phone.year === (selectedPhone.year || 0) - 1,
+      );
+      const nextYear = allPhones.filter(
+        (phone) => phone.year === (selectedPhone.year || 0) + 1,
+      );
+
+      setPhonesCurrentYear(currentYear.slice(0, 4));
+      setPhonesPrevYear(prevYear.slice(0, 4));
+      setPhonesNextYear(nextYear.slice(0, 4));
+    }
+  }, [allPhones, selectedPhone]);
+
+  const recommendedPhones = [
+    ...phonesNextYear,
+    ...phonesCurrentYear,
+    ...phonesPrevYear,
+  ];
+  const title = 'You may also like';
+
+  return <Slider selectedPhones={recommendedPhones} title={title} />;
 };
