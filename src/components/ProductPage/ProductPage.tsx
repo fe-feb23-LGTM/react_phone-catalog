3b470e08e9ae849ef06a9197e114dbff60046665
Oleksandr Bavdyk <sbavdik@gmail.com>
2023-06-16 14:03:00 +0300

    improved solution

--- src/components/ProductPage/ProductPage.tsx
+++ src/components/ProductPage/ProductPage.tsx
@@ -178,7 +178,7 @@ export const ProductPage = () => {
                     <div className="colors__item" key={color}>
                       <img
                         src={`icons/colors/${color}.svg`}
-                        alt="midnightColor"
+                        alt={`${color}`}
                         className="color"
                       />
                     </div>
