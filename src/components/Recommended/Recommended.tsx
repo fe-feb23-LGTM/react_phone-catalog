50792f217f733811f5c6537862915de0933a9454
Anastasiia Yuzyfyshyn <nastyajuzufichn@gmail.com>
2023-06-16 12:26:49 +0300

    fixed linter

--- src/components/Recommended/Recommended.tsx
+++ src/components/Recommended/Recommended.tsx
@@ -1,52 +1,17 @@
 import { useState, useEffect } from 'react';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import SwiperCore, { Navigation } from 'swiper';
 import { getPhones } from '../../api/phones';
 import { Phone } from '../../types/Phone';
-import { Card } from '../Card/Card';
-import './Recommended.scss';
+import { Slider } from '../Slider';
 
 export const Recommended = () => {
   const [phones, setPhones] = useState<Phone[]>([]);
+  const title = 'You may also like';
 
   useEffect(() => {
-    getPhones('price', '8', '1').then((data) => setPhones(data));
+    getPhones('price', '16', '1').then((data) => setPhones(data));
   }, []);
 
-  SwiperCore.use([Navigation]);
-
   return (
-    <div className="swiper-container">
-      <div className="swiper-button-prev" />
-      <div className="swiper-button-next" />
-      <Swiper
-        slidesPerView={2}
-        spaceBetween={16}
-        observer
-        observeParents
-        loop
-        navigation={{
-          prevEl: '.swiper-button-prev',
-          nextEl: '.swiper-button-next',
-        }}
-        breakpoints={{
-          640: {
-            slidesPerView: 2,
-          },
-          768: {
-            slidesPerView: 3,
-          },
-          1024: {
-            slidesPerView: 4,
-          },
-        }}
-      >
-        {phones.map((phone) => (
-          <SwiperSlide key={phone.id}>
-            <Card phone={phone} />
-          </SwiperSlide>
-        ))}
-      </Swiper>
-    </div>
+    <Slider selectedPhones={phones} title={title} />
   );
 };
