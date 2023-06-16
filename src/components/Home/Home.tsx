c256082b5b89f845836c66c8d25047c31aefb605
Andrii <remote.makandriival@gmail.com>
2023-06-16 11:34:11 +0300

    basycwork done

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,22 +1,59 @@
-import { Phone } from '../../types/Phone';
-import { Card } from '../Card/Card';
+// import { Phone } from '../../types/Phone';
 
 export const Home: React.FC = () => {
-  const phone: Phone = {
-    id: 1,
-    category: 'phones',
-    phoneId: 'apple-iphone-7-32gb-black',
-    itemId: 'apple-iphone-7-32gb-black',
-    name: 'Apple iPhone 7 32GB Black',
-    fullPrice: 400,
-    price: 375,
-    screen: "4.7' IPS",
-    capacity: '32GB',
-    color: 'black',
-    ram: '2GB',
-    year: 2016,
-    image: 'img/phones/apple-iphone-7/black/00.jpg',
-  };
+  return (
+    <div className="home__wrapper">
+      <div className="home">
+        <h1 className="home__heading">Welcome to Nice Gadgets Store</h1>
 
-  return <Card phone={phone} />;
+        <div className="home__topSlider">
+          Top slider!!! 340px high
+        </div>
+
+        <div className="home__newSlider">
+          brand new models!!! 500px high
+        </div>
+
+        <div className="home__categorys">
+          <h2 className="home__categorys__heading">Shop by category</h2>
+
+          <div className="home__categorys__wraper">
+            <div className="home__categorysItem">
+              <img
+                className="home__categorysItem__img img_bc_1"
+                src="img/catphones.png"
+                alt="categorysItemImg"
+              />
+              <h3 className="home__categorysItem__heading">Mobile phones</h3>
+              <span className="home__categorysItem__count">71 models</span>
+            </div>
+
+            <div className="home__categorysItem">
+              <img
+                className="home__categorysItem__img img_bc_2"
+                src="img/cattablets.png"
+                alt="categorysItemImg"
+              />
+              <h3 className="home__categorysItem__heading">Tablets</h3>
+              <span className="home__categorysItem__count">0 models</span>
+            </div>
+
+            <div className="home__categorysItem">
+              <img
+                className="home__categorysItem__img img_bc_3"
+                src="img/catacc.png"
+                alt="categorysItemImg"
+              />
+              <h3 className="home__categorysItem__heading">Accessories</h3>
+              <span className="home__categorysItem__count">0 models</span>
+            </div>
+          </div>
+        </div>
+
+        <div className="home__hotSlider">
+          hot Prices!!! 500px high
+        </div>
+      </div>
+    </div>
+  );
 };
